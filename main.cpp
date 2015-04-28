dcd6766543b2f506caec95f79c7f86805b3893a5
Simon Warta <simon@warta.it>
2015-04-28 20:31:25 +0200

    Add example

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <QFile>
+#include <QImage>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QFile file("86e231db24c2fd370992d91a9e889636.jpg");
+
+    if (!file.open(QIODevice::ReadOnly))
+    {
+        qFatal("Could not load sample image. Plase copy image file manually from source dir to build dir.");
+        return 1;
+    }
+
+    QByteArray blob = file.readAll();
+
+    QImage image;
+    image.fromData(blob);
+
+    return 0;
+}
